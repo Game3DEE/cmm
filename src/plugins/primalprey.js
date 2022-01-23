d07cd33960e6cca76435f7a5d43d5fbab9089fd5
Ithamar R. Adema <ithamar@upgrade-android.com>
2022-01-23 19:09:40 +0100

    PP: Scale up models, switch y/z axes

--- src/plugins/primalprey.js
+++ src/plugins/primalprey.js
@@ -14,6 +14,8 @@ import { DataType, Plugin } from './plugin.js'
 import { KaitaiStream } from 'kaitai-struct'
 import SSM from '../kaitai/primalprey_ssm.js'
 
+const scale = 32
+
 export class PrimalPreyPlugin extends Plugin {
     async loadFile(url, ext, baseName) {
         const model = this.loadModel(await this.loadFromURL(url), baseName)
@@ -43,9 +45,9 @@ export class PrimalPreyPlugin extends Plugin {
             }
             const a = f.vertices[0], b = f.vertices[1], c = f.vertices[2]
             position.push(
-                vertices[a].x, vertices[a].y, vertices[a].z,
-                vertices[b].x, vertices[b].y, vertices[b].z,
-                vertices[c].x, vertices[c].y, vertices[c].z,               
+                vertices[a].x * scale, vertices[a].z * scale, vertices[a].y * scale,
+                vertices[b].x * scale, vertices[b].z * scale, vertices[b].y * scale,
+                vertices[c].x * scale, vertices[c].z * scale, vertices[c].y * scale,
             )
             uv.push(
                 f.uvs[0], f.uvs[1],
@@ -65,7 +67,7 @@ export class PrimalPreyPlugin extends Plugin {
 
         const mat = []
         groups.forEach(g => {
-            const m = new MeshNormalMaterial({ side: DoubleSide })
+            const m = new MeshNormalMaterial()
             m.name = parsed.objects[g.materialIndex].name
             mat.push(m)
         })
