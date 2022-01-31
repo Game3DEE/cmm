cc2cf28c63dc84582051ec44e471982b0a73d2da
Ithamar R. Adema <ithamar@upgrade-android.com>
2022-01-31 20:06:26 +0100

    kaitai/vivisector: update CMF format with new findings
    most of blocks now have named IDs, found bone data referenced in
    TRK animation files (definition coming soon).

--- src/kaitai/vivisector_cmf.js
+++ src/kaitai/vivisector_cmf.js
@@ -7,26 +7,60 @@ VivisectorCmf.BlockId = Object.freeze({
     FACE_COUNT: 8209,
     VERT_COUNT: 8210,
     FACES: 8211,
+    FACE_UNKN_ATTR1_U4: 8215,
+    FACE_UNKN_ATTR6_U4: 8218,
+    FACE_MATERIALS: 8220,
+    FACE_UNKN_ATTR2_U4: 8221,
     UV1: 8224,
     UV2: 8225,
     VERTICES: 8227,
-    FLOATS: 8243,
+    SOMETHING_COUNT: 8240,
+    SOMETHING_UNKN_ATTR1_2X_U4: 8241,
+    SOMETHING_UNKN_ATTR2_2X_U4: 8242,
+    SOMETHING_UNKN_ATTR3_V3F: 8243,
     ALT_FACES: 8244,
     HEADER: 61441,
-    OBJECT_NAME: 61457,
+    BONE_COUNT: 61456,
+    BONE_NAMES: 61457,
+    BONE_POSITIONS: 61458,
+    BONE_UNKN_ATTR1_U2: 61459,
+    BONE_PARENTS: 61460,
+    BONE_TRANSFORMS: 61461,
+    FACE_UNKN_ATTR3_U4: 61473,
+    FACE_UNKN_ATTR4_U1: 61474,
+    FACE_UNKN_ATTR5_U2: 61475,
+    VERTEX_BONES: 61488,
+    VERTEX_UNKN_ATTR1_U2: 61489,
 
     2: "TEXTURE_COUNT",
     3: "TEXTURES",
     8209: "FACE_COUNT",
     8210: "VERT_COUNT",
     8211: "FACES",
+    8215: "FACE_UNKN_ATTR1_U4",
+    8218: "FACE_UNKN_ATTR6_U4",
+    8220: "FACE_MATERIALS",
+    8221: "FACE_UNKN_ATTR2_U4",
     8224: "UV1",
     8225: "UV2",
     8227: "VERTICES",
-    8243: "FLOATS",
+    8240: "SOMETHING_COUNT",
+    8241: "SOMETHING_UNKN_ATTR1_2X_U4",
+    8242: "SOMETHING_UNKN_ATTR2_2X_U4",
+    8243: "SOMETHING_UNKN_ATTR3_V3F",
     8244: "ALT_FACES",
     61441: "HEADER",
-    61457: "OBJECT_NAME",
+    61456: "BONE_COUNT",
+    61457: "BONE_NAMES",
+    61458: "BONE_POSITIONS",
+    61459: "BONE_UNKN_ATTR1_U2",
+    61460: "BONE_PARENTS",
+    61461: "BONE_TRANSFORMS",
+    61473: "FACE_UNKN_ATTR3_U4",
+    61474: "FACE_UNKN_ATTR4_U1",
+    61475: "FACE_UNKN_ATTR5_U2",
+    61488: "VERTEX_BONES",
+    61489: "VERTEX_UNKN_ATTR1_U2",
 });
 
 function VivisectorCmf(_io, _parent, _root) {
@@ -87,6 +121,26 @@ var Vector3f = VivisectorCmf.Vector3f = (function() {
     }
 
     return Vector3f;
+})();
+
+var SomethingUnknAttr3V3fBlock = VivisectorCmf.SomethingUnknAttr3V3fBlock = (function() {
+    function SomethingUnknAttr3V3fBlock(_io, _parent, _root) {
+    this._io = _io;
+    this._parent = _parent;
+    this._root = _root || this;
+
+    this._read();
+    }
+    SomethingUnknAttr3V3fBlock.prototype._read = function() {
+    this.vertices = [];
+    var i = 0;
+    while (!this._io.isEof()) {
+        this.vertices.push(new Vector3f(this._io, this, this._root));
+        i++;
+    }
+    }
+
+    return SomethingUnknAttr3V3fBlock;
 })();
 
 var UvBlock = VivisectorCmf.UvBlock = (function() {
@@ -154,6 +208,26 @@ var VertBlock = VivisectorCmf.VertBlock = (function() {
     return VertBlock;
 })();
 
+var BoneTransformsBlock = VivisectorCmf.BoneTransformsBlock = (function() {
+    function BoneTransformsBlock(_io, _parent, _root) {
+    this._io = _io;
+    this._parent = _parent;
+    this._root = _root || this;
+
+    this._read();
+    }
+    BoneTransformsBlock.prototype._read = function() {
+    this.boneTransforms = [];
+    var i = 0;
+    while (!this._io.isEof()) {
+        this.boneTransforms.push(new Matrix3x3f(this._io, this, this._root));
+        i++;
+    }
+    }
+
+    return BoneTransformsBlock;
+})();
+
 var TexturesBlock = VivisectorCmf.TexturesBlock = (function() {
     function TexturesBlock(_io, _parent, _root) {
     this._io = _io;
@@ -192,6 +266,26 @@ var Face = VivisectorCmf.Face = (function() {
     return Face;
 })();
 
+var BonePositionsBlock = VivisectorCmf.BonePositionsBlock = (function() {
+    function BonePositionsBlock(_io, _parent, _root) {
+    this._io = _io;
+    this._parent = _parent;
+    this._root = _root || this;
+
+    this._read();
+    }
+    BonePositionsBlock.prototype._read = function() {
+    this.bonePositions = [];
+    var i = 0;
+    while (!this._io.isEof()) {
+        this.bonePositions.push(new Vector3f(this._io, this, this._root));
+        i++;
+    }
+    }
+
+    return BonePositionsBlock;
+})();
+
 var Block = VivisectorCmf.Block = (function() {
     function Block(_io, _parent, _root) {
     this._io = _io;
@@ -212,18 +306,39 @@ var Block = VivisectorCmf.Block = (function() {
     case VivisectorCmf.BlockId.VERT_COUNT:
         this.data = this._io.readU4le();
         break;
-    case VivisectorCmf.BlockId.FLOATS:
-        this._raw_data = this._io.readBytes(this.size);
-        var _io__raw_data = new KaitaiStream(this._raw_data);
-        this.data = new FloatsBlock(_io__raw_data, this, this._root);
+    case VivisectorCmf.BlockId.SOMETHING_COUNT:
+        this.data = this._io.readU4le();
         break;
     case VivisectorCmf.BlockId.TEXTURE_COUNT:
         this.data = this._io.readU4le();
+        break;
+    case VivisectorCmf.BlockId.BONE_NAMES:
+        this._raw_data = this._io.readBytes(this.size);
+        var _io__raw_data = new KaitaiStream(this._raw_data);
+        this.data = new BoneNamesBlock(_io__raw_data, this, this._root);
         break;
     case VivisectorCmf.BlockId.ALT_FACES:
         this._raw_data = this._io.readBytes(this.size);
         var _io__raw_data = new KaitaiStream(this._raw_data);
         this.data = new FacesBlock(_io__raw_data, this, this._root);
+        break;
+    case VivisectorCmf.BlockId.VERTEX_BONES:
+        this._raw_data = this._io.readBytes(this.size);
+        var _io__raw_data = new KaitaiStream(this._raw_data);
+        this.data = new VertexBonesBlock(_io__raw_data, this, this._root);
+        break;
+    case VivisectorCmf.BlockId.BONE_COUNT:
+        this.data = this._io.readU4le();
+        break;
+    case VivisectorCmf.BlockId.SOMETHING_UNKN_ATTR3_V3F:
+        this._raw_data = this._io.readBytes(this.size);
+        var _io__raw_data = new KaitaiStream(this._raw_data);
+        this.data = new SomethingUnknAttr3V3fBlock(_io__raw_data, this, this._root);
+        break;
+    case VivisectorCmf.BlockId.BONE_POSITIONS:
+        this._raw_data = this._io.readBytes(this.size);
+        var _io__raw_data = new KaitaiStream(this._raw_data);
+        this.data = new BonePositionsBlock(_io__raw_data, this, this._root);
         break;
     case VivisectorCmf.BlockId.TEXTURES:
         this._raw_data = this._io.readBytes(this.size);
@@ -232,6 +347,11 @@ var Block = VivisectorCmf.Block = (function() {
         break;
     case VivisectorCmf.BlockId.FACE_COUNT:
         this.data = this._io.readU4le();
+        break;
+    case VivisectorCmf.BlockId.FACE_MATERIALS:
+        this._raw_data = this._io.readBytes(this.size);
+        var _io__raw_data = new KaitaiStream(this._raw_data);
+        this.data = new FaceMaterialsBlock(_io__raw_data, this, this._root);
         break;
     case VivisectorCmf.BlockId.HEADER:
         this._raw_data = this._io.readBytes(this.size);
@@ -243,13 +363,20 @@ var Block = VivisectorCmf.Block = (function() {
         var _io__raw_data = new KaitaiStream(this._raw_data);
         this.data = new UvBlock(_io__raw_data, this, this._root);
         break;
+    case VivisectorCmf.BlockId.BONE_TRANSFORMS:
+        this._raw_data = this._io.readBytes(this.size);
+        var _io__raw_data = new KaitaiStream(this._raw_data);
+        this.data = new BoneTransformsBlock(_io__raw_data, this, this._root);
+        break;
     case VivisectorCmf.BlockId.FACES:
         this._raw_data = this._io.readBytes(this.size);
         var _io__raw_data = new KaitaiStream(this._raw_data);
         this.data = new FacesBlock(_io__raw_data, this, this._root);
         break;
-    case VivisectorCmf.BlockId.OBJECT_NAME:
-        this.data = KaitaiStream.bytesToStr(KaitaiStream.bytesTerminate(this._io.readBytes(this.size), 0, false), "utf8");
+    case VivisectorCmf.BlockId.BONE_PARENTS:
+        this._raw_data = this._io.readBytes(this.size);
+        var _io__raw_data = new KaitaiStream(this._raw_data);
+        this.data = new BoneParentsBlock(_io__raw_data, this, this._root);
         break;
     case VivisectorCmf.BlockId.VERTICES:
         this._raw_data = this._io.readBytes(this.size);
@@ -263,6 +390,44 @@ var Block = VivisectorCmf.Block = (function() {
     }
 
     return Block;
+})();
+
+var BoneNamesBlock = VivisectorCmf.BoneNamesBlock = (function() {
+    function BoneNamesBlock(_io, _parent, _root) {
+    this._io = _io;
+    this._parent = _parent;
+    this._root = _root || this;
+
+    this._read();
+    }
+    BoneNamesBlock.prototype._read = function() {
+    this.boneNames = [];
+    var i = 0;
+    while (!this._io.isEof()) {
+        this.boneNames.push(KaitaiStream.bytesToStr(KaitaiStream.bytesTerminate(this._io.readBytes(32), 0, false), "utf8"));
+        i++;
+    }
+    }
+
+    return BoneNamesBlock;
+})();
+
+var Matrix3x3f = VivisectorCmf.Matrix3x3f = (function() {
+    function Matrix3x3f(_io, _parent, _root) {
+    this._io = _io;
+    this._parent = _parent;
+    this._root = _root || this;
+
+    this._read();
+    }
+    Matrix3x3f.prototype._read = function() {
+    this.elements = new Array(9);
+    for (var i = 0; i < 9; i++) {
+        this.elements[i] = this._io.readF4le();
+    }
+    }
+
+    return Matrix3x3f;
 })();
 
 var FacesBlock = VivisectorCmf.FacesBlock = (function() {
@@ -285,24 +450,73 @@ var FacesBlock = VivisectorCmf.FacesBlock = (function() {
     return FacesBlock;
 })();
 
-var FloatsBlock = VivisectorCmf.FloatsBlock = (function() {
-    function FloatsBlock(_io, _parent, _root) {
+var VertexBonesBlock = VivisectorCmf.VertexBonesBlock = (function() {
+    function VertexBonesBlock(_io, _parent, _root) {
     this._io = _io;
     this._parent = _parent;
     this._root = _root || this;
 
     this._read();
     }
-    FloatsBlock.prototype._read = function() {
-    this.vertices = [];
+    VertexBonesBlock.prototype._read = function() {
+    this.boneIndices = [];
     var i = 0;
     while (!this._io.isEof()) {
-        this.vertices.push(new Vector3f(this._io, this, this._root));
+        this.boneIndices.push(this._io.readU2le());
         i++;
     }
     }
 
-    return FloatsBlock;
+    /**
+     * Maps every vertex to a bone
+     */
+
+    return VertexBonesBlock;
 })();
+
+var FaceMaterialsBlock = VivisectorCmf.FaceMaterialsBlock = (function() {
+    function FaceMaterialsBlock(_io, _parent, _root) {
+    this._io = _io;
+    this._parent = _parent;
+    this._root = _root || this;
+
+    this._read();
+    }
+    FaceMaterialsBlock.prototype._read = function() {
+    this.indices = [];
+    var i = 0;
+    while (!this._io.isEof()) {
+        this.indices.push(this._io.readU4le());
+        i++;
+    }
+    }
+
+    /**
+     * Maps every face to a material (texture)
+     */
+
+    return FaceMaterialsBlock;
+})();
+
+var BoneParentsBlock = VivisectorCmf.BoneParentsBlock = (function() {
+    function BoneParentsBlock(_io, _parent, _root) {
+    this._io = _io;
+    this._parent = _parent;
+    this._root = _root || this;
+
+    this._read();
+    }
+    BoneParentsBlock.prototype._read = function() {
+    this.boneParents = [];
+    var i = 0;
+    while (!this._io.isEof()) {
+        this.boneParents.push(this._io.readS2le());
+        i++;
+    }
+    }
+
+    return BoneParentsBlock;
+})();
+  
 
 export default VivisectorCmf
--- src/kaitai/vivisector_cmf.ksy
+++ src/kaitai/vivisector_cmf.ksy
@@ -30,16 +30,30 @@ types:
             block_id::faces: faces_block
             block_id::header: header_block
             block_id::texture_count: u4
+            block_id::bone_count: u4
             block_id::face_count: u4
             block_id::vert_count: u4
+            block_id::something_count: u4
             block_id::textures: textures_block
             block_id::uv1: uv_block
             block_id::uv2: uv_block
-            block_id::object_name: strz
+            block_id::bone_names: bone_names_block
+            block_id::bone_positions: bone_positions_block
+            block_id::bone_parents: bone_parents_block
+            block_id::bone_transforms: bone_transforms_block
             block_id::alt_faces: faces_block
-            block_id::floats: floats_block
+            block_id::something_unkn_attr3_v3f: something_unkn_attr3_v3f_block
+            block_id::face_materials: face_materials_block
+            block_id::vertex_bones: vertex_bones_block
 
-  floats_block:
+  face_materials_block:
+    seq:
+      - id: indices
+        type: u4
+        doc: Maps every face to a material (texture)
+        repeat: eos # matches number of faces
+
+  something_unkn_attr3_v3f_block:
     seq:
       - id: vertices
         type: vector3f
@@ -82,7 +96,39 @@ types:
       - id: vertices
         type: vector3f
         repeat: eos
-        
+
+  vertex_bones_block:
+    seq:
+      - id: bone_indices
+        type: u2
+        doc: Maps every vertex to a bone
+        repeat: eos # matches number of vertices
+  
+  bone_names_block:
+    seq:
+      - id: bone_names
+        type: strz
+        size: 32
+        repeat: eos # matches number of bones
+
+  bone_positions_block:
+    seq:
+      - id: bone_positions
+        type: vector3f
+        repeat: eos # matches number of bones
+
+  bone_parents_block:
+    seq:
+      - id: bone_parents
+        type: s2
+        repeat: eos # matches number of bones
+
+  bone_transforms_block:
+    seq:
+      - id: bone_transforms
+        type: matrix3x3f
+        repeat: eos # matches number of bones
+
   faces_block:
     seq:
       - id: faces
@@ -134,6 +180,13 @@ types:
       - id: z
         type: f4
 
+  matrix3x3f:
+    seq:
+      - id: elements
+        type: f4
+        repeat: expr
+        repeat-expr: 9
+
 enums:
   block_id:
     0x0002: texture_count
@@ -141,10 +194,27 @@ enums:
     0xf001: header
     0x2011: face_count
     0x2012: vert_count
+    0xf010: bone_count
     0x2013: faces
+    0x2017: face_unkn_attr1_u4
+    0x201a: face_unkn_attr6_u4
+    0x201c: face_materials
+    0x201d: face_unkn_attr2_u4
     0x2020: uv1
     0x2021: uv2
     0x2023: vertices
-    0x2033: floats
+    0x2030: something_count
+    0x2031: something_unkn_attr1_2x_u4
+    0x2032: something_unkn_attr2_2x_u4
+    0x2033: something_unkn_attr3_v3f
     0x2034: alt_faces
-    0xf011: object_name
+    0xf011: bone_names
+    0xf012: bone_positions
+    0xf013: bone_unkn_attr1_u2
+    0xf014: bone_parents
+    0xf015: bone_transforms
+    0xf021: face_unkn_attr3_u4
+    0xf022: face_unkn_attr4_u1
+    0xf023: face_unkn_attr5_u2
+    0xf030: vertex_bones
+    0xf031: vertex_unkn_attr1_u2
