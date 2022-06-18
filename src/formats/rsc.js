e3c6fd5551d89a1bcd54067084e2276d1cff8e56
Ithamar R. Adema <ithamar@upgrade-android.com>
2022-06-18 20:15:52 +0200

    formats/rsc: fix C2 support and add saveRSC code

--- src/formats/rsc.js
+++ src/formats/rsc.js
@@ -9,8 +9,9 @@ export const of = {
   Animated: 0x80000000,
 }
 
+const textureDim = 128
+
 export function loadRSC(buffer, version) {
-  const textureDim = 128
   const dv = new DataView(buffer)
   let offset = 0
 
@@ -78,8 +79,19 @@ export function loadRSC(buffer, version) {
       objects.push(objInfo)
   }
 
-  const skyTexture = new Uint16Array(buffer, offset, 256 * 256)
-  offset += 256 * 256 * 2
+  let dawnSkyTexture, daySkyTexture, nightSkyTexture
+  if (version === 1) {
+    dawnSkyTexture = daySkyTexture = nightSkyTexture = new Uint16Array(buffer, offset, 256 * 256)
+    offset += 256 * 256 * 2
+  } else {
+    dawnSkyTexture = new Uint16Array(buffer, offset, 256 * 256)
+    offset += 256 * 256 * 2
+    daySkyTexture = new Uint16Array(buffer, offset, 256 * 256)
+    offset += 256 * 256 * 2
+    nightSkyTexture = new Uint16Array(buffer, offset, 256 * 256)
+    offset += 256 * 256 * 2
+  }
+
   const cloudMap = new Uint8ClampedArray(buffer, offset, 128 * 128)
   offset += 128 * 128
 
@@ -144,6 +156,20 @@ export function loadRSC(buffer, version) {
       })
   }
 
+  const waterCount = dv.getUint32(offset, true)
+  offset += 4
+
+  const waters = []
+  for (let i = 0; i < waterCount; i++) {
+    waters.push({
+      textureIndex: dv.getUint32(offset + 0, true),
+      level: dv.getInt32(offset + 4, true),
+      opacity: dv.getFloat32(offset + 8, true),
+      color: dv.getUint32(offset + 12, true),
+    })
+    offset += 16
+  }
+
   return {
     textureCount,
     textureDim,
@@ -153,10 +179,145 @@ export function loadRSC(buffer, version) {
     nightSkyRGB, nightSkyTRGB,
     textures,
     objects,
-    skyTexture,
+    dawnSkyTexture,
+    daySkyTexture,
+    nightSkyTexture,
     cloudMap,
     fogs,
     randomSounds,
     ambientSounds,
   }
+}
+
+export function saveRSC(rsc) {
+  const objectDataSize = 0 //rsc.objects.length * xxx
+  const soundDataSize = rsc.randomSounds.reduce((p,rs) => p + rs.length, 0)
+  const ambSoundDataSize = rsc.ambientSounds.reduce((p,as) => p + as.audio.length, 0)
+  const size = 3 * 12 + // SkyRGB
+              3 * 12 + // SkyTRGB
+              4 + 4 + // tex count + model count
+              rsc.textures.length * 2 + // textures
+              objectDataSize + // objects
+              256 * 256 * 2 * 3 + // sky textures [dawn,day,night]
+              128 * 128 + // cloud map
+              4 + rsc.fogs.length * 20 +
+              4 + soundDataSize + rsc.randomSounds.length * 4 + // ramdom audio samples
+              4 + ambSoundDataSize + rsc.ambientSounds.length * (256 + 12) + // rData + pcmSize
+              4 + rsc.waters.length * 16 // water table
+
+  const buf = new ArrayBuffer(size)
+  const dv = new DataView(buf)
+  let offset = 0
+
+  function writeRGB(arr) {
+    dv.setUint32(offset + 0, arr[0], true)
+    dv.setUint32(offset + 4, arr[1], true)
+    dv.setUint32(offset + 8, arr[2], true)
+    offset += 12
+  }
+
+  // write tex & obj count
+  dv.setUint32(offset + 0, rsc.textures.length / (textureDim * textureDim), true)
+  dv.setUint32(offset + 4, rsc.objects.length, true)
+  offset += 8
+
+  // write sky colors
+  writeRGB(rsc.dawnSkyRGB); writeRGB(rsc.daySkyRGB); writeRGB(rsc.nightSkyRGB)
+  writeRGB(rsc.dawnSkyTRGB); writeRGB(rsc.daySkyTRGB); writeRGB(rsc.nightSkyTRGB)
+
+  // write terrain texture data
+  for (let i = 0; i < rsc.textures.length; i++) {
+    dv.setUint16(offset, rsc.textures[i], true)
+    offset += 2
+  }
+
+  // write objects
+  for (let i = 0; i < rsc.objects.length; i++) {
+    // TODO XXX implement!
+  }
+
+  // Write sky textures
+  for (let i = 0; i < rsc.dawnSkyTexture.length; i++) {
+    dv.setUint16(offset, rsc.dawnSkyTexture[i], true)
+    offset += 2
+  }
+  for (let i = 0; i < rsc.daySkyTexture.length; i++) {
+    dv.setUint16(offset, rsc.daySkyTexture[i], true)
+    offset += 2
+  }
+  for (let i = 0; i < rsc.nightSkyTexture.length; i++) {
+    dv.setUint16(offset, rsc.nightSkyTexture[i], true)
+    offset += 2
+  }
+
+  // Write cloud map
+  for (let i = 0; i < rsc.cloudMap.length; i++) {
+    dv.setUint8(offset, rsc.cloudMap[i])
+    ++offset
+  }
+
+  // Write Fog list
+  dv.setUint32(offset, rsc.fogs?.length || 0, true)
+  offset += 4
+  rsc.fogs?.forEach(f => {
+    dv.setUint32(offset + 0, f.color, true),
+    dv.setFloat32(offset + 4, f.yBegin, true),
+    dv.setUint32(offset + 8, f.mortal, true),
+    dv.setFloat32(offset + 12, f.transp, true),
+    dv.setFloat32(offset + 16, f.fLimit, true),
+    offset += 20
+  })
+
+  // Write random sounds
+  dv.setUint32(offset, rsc.randomSounds.length, true)
+  offset += 4
+  rsc.randomSounds?.forEach(rs => {
+    dv.setUint32(offset, rs.length, true)
+    offset += 4
+    for (let i = 0; i< rs.length; i++) {
+      dv.setUint8(offset, rs[i])
+      ++offset
+    }
+  })
+
+  // Write ambient sounds
+  dv.setUint32(offset, rsc.ambientSounds?.length || 0, true)
+  offset += 4
+  rsc.ambientSounds?.forEach(as => {
+    console.log(as.audio)
+    dv.setUint32(offset, as.audio.length, true)
+    offset += 4
+    for (let i = 0; i < as.audio.length; i++) {
+      dv.setUint8(offset, as.audio[i])
+      ++offset
+    }
+    const emptyRandom = { number: 0, volume: 0, frequency: 0, environment: 0 }
+    for (let i = 0; i < 16; i++) {
+      const rdata = as.randoms?.[i] || emptyRandom
+      dv.setUint32(offset + 0, rdata.number, true),
+      dv.setUint32(offset + 4, rdata.volume, true),
+      dv.setUint32(offset + 8, rdata.frequency, true)
+      dv.setUint16(offset + 12, rdata.environment, true)
+      dv.setUint16(offset + 14, 0, true)
+      offset += 16
+    }
+    dv.setUint32(offset + 0, as.randoms?.length || 0, true)
+    dv.setUint32(offset + 4, as.volume, true)
+    offset += 8
+  })
+
+  // Write waters
+  dv.setUint32(offset, rsc.waters.length, true)
+  offset += 4
+  rsc.waters.forEach(w => {
+    dv.setUint32(offset + 0, w.textureIndex, true)
+    dv.setInt32(offset + 4, w.level, true)
+    dv.setFloat32(offset + 8, w.opacity, true)
+    dv.setUint32(offset + 12, w.color, true)
+    offset += 16
+  })
+
+  //DEBUG: console.log(offset, buf.byteLength)
+
+  return buf
 }
